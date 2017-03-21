d9d11387a031580728d9294e06e8d68428e84d1e
Vladimir V. Ivanov <help@dr.support>
2017-03-21 16:48:29 +0300

    fix

--- app/views/assets/js/app/offer_0_1.js
+++ app/views/assets/js/app/offer_0_1.js
@@ -1,21 +1,48 @@
-
-
-function convertImgToBase64URL(url, callback, outputFormat){
-    var img = new Image();
-    img.crossOrigin = 'Anonymous';
-    img.onload = function(){
-        var canvas = document.createElement('CANVAS'),
-        ctx = canvas.getContext('2d'), dataURL;
-        canvas.height = img.height;
-        canvas.width = img.width;
-        ctx.drawImage(img, 0, 0);
-        dataURL = canvas.toDataURL(outputFormat);
-        callback(dataURL);
-        canvas = null; 
-    };
-    img.src = url;
-}
-convertImgToBase64URL('http://na.captcha.yandex.net/image?key=c3STbwbpvMK66r1XZTcUI7CQtyrqz6QS', function(base64Img){
-    console.log(base64Img);
-   
-});
+document.addEventListener("DOMContentLoaded", function(){ 
+    var f = document.getElementsByClassName('createForm')[0];  
+    var e = document.querySelector('[type="submit"]');
+    var p = document.getElementsByClassName('preloader')[0];
+    var a = document.getElementsByClassName('alert')[0];
+    var m = document.getElementsByClassName('msg')[0];
+    if(validateForm(f)) {     
+        e.className += " btn-info";
+        e.classList.remove("btn-default");
+        e.disabled = false;
+    } else {
+        e.className += " btn-default";
+        e.classList.remove("btn-info");
+        e.disabled = true;            
+    }  
+    f.addEventListener("keyup", function(event) {
+        event.preventDefault();          
+        e.disabled = false;       
+        if(validateForm(f)) {     
+            e.className += " btn-info";
+            e.classList.remove("btn-default");
+            e.disabled = false;
+        } else {
+            e.className += " btn-default";
+            e.classList.remove("btn-info");
+            e.disabled = true;            
+        }        
+    });
+    f.addEventListener("submit", function(event) {   
+        event.preventDefault();           
+        p.style.display = 'block'; 
+        e.disabled = true;
+        var arg = serialize(this);            
+        ajax('/admin/offer/create', arg, function(data){
+            if(isValidJSON(data)) var data = JSON.parse(data);
+            if(data['response']['success']) { 
+                window.location = '/admin/offer/';
+            } else {            
+                p.style.display = 'none';
+                e.className += " btn-default";
+                e.classList.remove("btn-info"); 
+                a.style.display = 'block';
+                m.innerHTML = data['response']['msg'];
+                f.elements[data['response']['target']].style.borderColor = '#ffb400';
+            }
+        }); 
+    });             
+}, false);
