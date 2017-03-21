a542c64d23b903468c876aaaece5836bb40023e1
Vladimir V. Ivanov <help@dr.support>
2017-03-21 19:11:40 +0300

    fix

--- app/controllers/YWS.php
+++ app/controllers/YWS.php
@@ -23,39 +23,34 @@ class YWSController extends Controller {
 		return $data;
 	}
     function index() {
-    	/*require_once(__DIR__.'../../../vendor/ivansky/php-yandex-wordstat/YADWord.php');
-    	require_once(__DIR__.'../../../vendor/ivansky/php-yandex-wordstat/YADWordstat.php');*/
-
-    	/*$this->db->exec("TRUNCATE TABLE  `tbl_logs`");
-    	$this->db->exec("TRUNCATE TABLE  `tbl_stats`");*/    	
- 		
- 		//$this->f3->get('PARAMS.id');
-
     	$word = $this->db->exec("SELECT tbl_words.name, tbl_words.id, stat.datetime FROM tbl_words LEFT JOIN( SELECT tbl_stats.datetime, tbl_stats.word FROM tbl_stats WHERE tbl_stats.datetime = CURDATE() GROUP BY tbl_stats.word) AS stat ON stat.word = tbl_words.id ORDER BY stat.datetime ASC LIMIT 1")[0];
     	if(!$word) $this->pushJSON(false, "words empty");
 
     	$time = date('Y-m-d H:i:s', mktime(date("H"), date("i"), date("s"), date("m"), date("d"), date("Y"))); 		
 		$this->db->exec("INSERT INTO `tbl_logs`( `id` , `datetime` , `status`, `query`, `response`) VALUES ( NULL , '".$time."', '0', '', '".$word['name']."');");
 		$log = $this->db->lastInsertId();	
-
-		//account
         $setting = $this->db->exec("SELECT * FROM  `tbl_settings`")[0];
 
-		$query = './vendor/ariya/phantomjs/bin/phantomjs yws.js "'.$word['name'].'" '.$log.' '.$setting['value1'].' '.$setting['value2'].' 2>&1';
+		//$setting['value1']
+		$query = './vendor/drsupport/parser.yws/vendor/ariya/phantomjs/bin/phantomjs --web-security=no ./vendor/drsupport/parser.yws/yws.min.enc.js 003fa7c1cd658bca6016eae7c179f012 ivanov.vladimir.v sp@rt@nec "'.$word['name'].'" "desktop" "weekly" "225" 2>&1';
 
-
-        $this->db->exec("UPDATE  `capito`.`tbl_logs` SET  `query` =  '".$query."' WHERE  `tbl_logs`.`id` =".$log.";"); 
- 		//foreach ($words as $key=>$word) {   	} 
+        $this->db->exec("UPDATE  `tbl_logs` SET  `query` =  '".$query."' WHERE  `tbl_logs`.`id` =".$log.";"); 
  		putenv('LANG=en_US.UTF-8'); 
         $response = shell_exec($query); 
 
         if(empty($response) OR !$this->isJson($response)) $response = '';        
         $this->db->exec("UPDATE  `tbl_logs` SET  `status` =  '".strval(empty($response) ? '-1' : '1')."' WHERE  `tbl_logs`.`id` =".$log.";");   
 
-        $response = json_decode($response);
-        $this->db->exec("INSERT IGNORE INTO  `tbl_stats` (`id`, `datetime`, `word`, `device`, `geo`, `impressions`) VALUES (NULL, '".date("Y-m-d 00:00", strtotime($response->datetime))."', '".$word['id']."', NULL, NULL, '".$response->impressions."');");
+        $response = json_decode($response);   
+         
+		foreach($response->response->history as $key => $history){ 
+			echo $history->period.$history->value;
+			//$this->db->exec("INSERT INTO `tbl_analytic` (`id`, `datetime_in`, `datetime_out`, `word`, `db`, `period`, `regions`, `impressions`) VALUES (NULL, '2017-03-01', '2017-03-31', '1', 'mobile', 'weekly', '255', '10');");
+		}    
 
-		$this->get('http://capito.dr.cash/p/stat');
+        //$this->db->exec("INSERT IGNORE INTO  `tbl_stats` (`id`, `datetime`, `word`, `device`, `geo`, `impressions`) VALUES (NULL, '".date("Y-m-d 00:00", strtotime($response->datetime))."', '".$word['id']."', NULL, NULL, '".$response->impressions."');");
+
+		//$this->get('http://capito.dr.cash/p/stat');
 		//$this->pushJSON(true, $word['name']); 
         
 
